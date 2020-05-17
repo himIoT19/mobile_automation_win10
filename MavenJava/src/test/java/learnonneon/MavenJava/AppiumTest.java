9a9232e3897f74a060e7ebb8108b800eddd6f1fe
himIoT19 <thehimu1990@gmail.com>
2020-05-18 02:31:02 +0530

    new project using maven and testng

---
+++ MavenJava/src/test/java/learnonneon/MavenJava/AppiumTest.java
@@ -0,0 +1,16 @@
+package learnonneon.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeAppAndroid() {
+		System.out.println("Native apps Android");
+	}
+
+	@Test
+	public void iOSApps() {
+		System.out.println("iOS apps iPhone");
+	}
+}
---
+++ MavenJava/src/test/java/learnonneon/MavenJava/RESTAPITest.java
@@ -0,0 +1,16 @@
+package learnonneon.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void postJira() {
+		System.out.println("POST Jira");
+	}
+
+	@Test
+	public void deleteTwitter() {
+		System.out.println("Delete Twitter");
+	}
+}
